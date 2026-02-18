e549dcabc3f920f06c3ad7a5200d0c25ec9cc6ed
Shruti Dhere <dhereshruti351@gmail.com>
2026-02-18 21:18:44 +0530

    Add files via upload

---
+++ question_11.java
@@ -0,0 +1,11 @@
+//11. int a = 8, b = 2; System.out.println(a >> b);
+
+package Guess_The_Output;
+
+public class question_11 {
+      public static void main(String[] args) {
+    	  int a = 8;
+    	  int b = 2;
+    	  System.out.println(a >> b);    // 2
+	}
+}
